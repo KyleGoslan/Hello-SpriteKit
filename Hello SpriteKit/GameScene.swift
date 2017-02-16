4972f57a539327d10a7a100e05280e5e73fd5a66
KyleGoslan <kylegoslan@me.com>
2017-02-16 12:02:21 +0000

    Remove some stuff

--- Hello SpriteKit/GameScene.swift
+++ Hello SpriteKit/GameScene.swift
@@ -2,21 +2,12 @@ import SpriteKit
 
 class GameScene: SKScene {
   
-  var player: SKSpriteNode!
-  
   override func didMove(to view: SKView) {
-    player = SKSpriteNode(imageNamed: "Spaceship")
-    addChild(player)
+
   }
     
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     guard let touch = touches.first else { return }
-    
-    let touchPositon = touch.location(in: self)
-    
-    let duration = getDuration(pointA: player.position, pointB: touchPositon, speed: 500)
-    let move = SKAction.move(to: touchPositon, duration: duration)
-    player.run(move)
     
   }
   
@@ -35,13 +26,5 @@ class GameScene: SKScene {
   override func update(_ currentTime: TimeInterval) {
       
   }
-  
-  func getDuration(pointA: CGPoint, pointB: CGPoint, speed: CGFloat) -> TimeInterval {
-    let xDist = (pointB.x - pointA.x)
-    let yDist = (pointB.y - pointA.y)
-    let distance = sqrt((xDist * xDist) + (yDist * yDist));
-    let duration = TimeInterval(distance/speed)
-    return duration
-  }
-  
+
 }
